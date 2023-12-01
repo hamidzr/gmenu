0fd64079c63171cfa358515e713afa7572e8a293
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-12-01 00:00:00 -0800

    first working pass

--- menu.go
+++ menu.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -15,29 +17,52 @@ type MenuItem struct {
 }
 
 type Menu struct {
-	Items []MenuItem
+	Items      []MenuItem
+	Filtered   []MenuItem
+	Selected   int
+	ResultText string
 }
 
 func NewMenu() Menu {
 	items := []MenuItem{
-		{1, "Item 1"},
-		{2, "Item 2"},
-		{3, "Item 3"},
-		{4, "Item 4"},
+		{1, "Item pp 1"},
+		{2, "Item pp 2"},
+		{3, "Item abp 3"},
+		{4, "Item gga 4"},
 		{5, "Item 5"},
 	}
-
-	return Menu{Items: items}
+	if len(items) == 0 {
+		panic("Menu must have at least one item")
+	}
+	return Menu{Items: items, Filtered: items, Selected: 0}
 }
 
-func (m Menu) Search(keyword string) []MenuItem {
-	var results []MenuItem
+func (m *Menu) Search(keyword string) {
+	m.Filtered = nil // Reset the filtered list
 	for _, item := range m.Items {
 		if strings.Contains(strings.ToLower(item.Title), strings.ToLower(keyword)) {
-			results = append(results, item)
+			m.Filtered = append(m.Filtered, item)
 		}
 	}
-	return results
+	m.UpdateResultText()
+}
+
+func (m *Menu) UpdateResultText() {
+	// set result text to the first filtered item if any
+	if len(m.Filtered) > 0 {
+		m.Selected = 0
+	} else {
+		m.ResultText = ""
+	}
+
+	m.ResultText = "\n"
+	for i, item := range m.Filtered {
+		if i == m.Selected {
+			m.ResultText += fmt.Sprintf("-> [%d] %s\n", item.ID, item.Title)
+		} else {
+			m.ResultText += fmt.Sprintf("   [%d] %s\n", item.ID, item.Title)
+		}
+	}
 }
 
 func main() {
@@ -49,25 +74,42 @@ func main() {
 	searchEntry := widget.NewEntry()
 	searchEntry.SetPlaceHolder("Search")
 
-	resultLabel := widget.NewMultiLineEntry()
-	resultLabel.Disable()
+	resultLabel := widget.NewLabel("")
+	resultLabel.Wrapping = fyne.TextWrapWord
 
-	// Update results as text changes
 	searchEntry.OnChanged = func(text string) {
-		results := menu.Search(text)
-
-		resultText := ""
-		for _, item := range results {
-			resultText += fmt.Sprintf("[%d] %s\n", item.ID, item.Title)
-		}
-
-		resultLabel.SetText(resultText)
+		menu.Search(text)
+		resultLabel.SetText(menu.ResultText)
 	}
+	searchEntry.OnSubmitted = func(text string) {
+		fmt.Println("Submitted", text)
+	}
+	// TODO: listen to arrow keys on searchEntry
+
+	myWindow.Canvas().SetOnTypedKey(func(keyEvent *fyne.KeyEvent) {
+		switch keyEvent.Name {
+		case fyne.KeyDown:
+			if menu.Selected < len(menu.Filtered)-1 {
+				menu.Selected++
+			}
+		case fyne.KeyUp:
+			if menu.Selected > 0 {
+				menu.Selected--
+			}
+		case fyne.KeyReturn:
+			if menu.Selected >= 0 && menu.Selected < len(menu.Filtered) {
+				fmt.Fprintln(os.Stdout, menu.Filtered[menu.Selected].Title)
+				myApp.Quit()
+			}
+		}
+		menu.UpdateResultText()
+		resultLabel.SetText(menu.ResultText)
+	})
 
 	menuLabel := widget.NewLabel("Menu:")
-	searchContainer := container.NewHBox(searchEntry)
-	contentContainer := container.NewVBox(menuLabel, resultLabel)
+	contentContainer := container.NewBorder(nil, nil, nil, nil, menuLabel, resultLabel)
 
-	myWindow.SetContent(container.NewVBox(searchContainer, contentContainer))
+	myWindow.SetContent(container.NewVBox(searchEntry, contentContainer))
+	myWindow.Resize(fyne.NewSize(400, 300)) // Adjust as needed
 	myWindow.ShowAndRun()
 }
