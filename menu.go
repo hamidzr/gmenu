c7683b3cfd5bddd9a54ecec523d9e4566c2d3ac4
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-12-01 00:00:00 -0800

    init

---
+++ menu.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+type MenuItem struct {
+	ID    int
+	Title string
+}
+
+type Menu struct {
+	Items []MenuItem
+}
+
+func NewMenu() Menu {
+	items := []MenuItem{
+		{1, "Item 1"},
+		{2, "Item 2"},
+		{3, "Item 3"},
+		{4, "Item 4"},
+		{5, "Item 5"},
+	}
+
+	return Menu{Items: items}
+}
+
+func (m Menu) Search(keyword string) []MenuItem {
+	var results []MenuItem
+	for _, item := range m.Items {
+		if strings.Contains(strings.ToLower(item.Title), strings.ToLower(keyword)) {
+			results = append(results, item)
+		}
+	}
+	return results
+}
+
+func main() {
+	menu := NewMenu()
+
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Menu")
+
+	searchEntry := widget.NewEntry()
+	searchEntry.SetPlaceHolder("Search")
+
+	searchButton := widget.NewButton("Search", func() {
+		keyword := searchEntry.Text
+		results := menu.Search(keyword)
+
+		resultText := ""
+		for _, item := range results {
+			resultText += fmt.Sprintf("[%d] %s\n", item.ID, item.Title)
+		}
+
+		resultLabel.SetText(resultText)
+	})
+
+	menuLabel := widget.NewLabel("Menu:")
+	resultLabel := widget.NewMultiLineEntry()
+	resultLabel.Disable()
+
+	searchContainer := container.NewHBox(searchEntry, searchButton)
+	contentContainer := container.NewVBox(menuLabel, resultLabel)
+
+	myWindow.SetContent(container.NewVBox(searchContainer, contentContainer))
+	myWindow.ShowAndRun()
+}
