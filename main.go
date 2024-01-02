a18ba21b01dedf4d541bfd9f74d80189f84aede7
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2024-01-02 00:00:00 -0800

    separate main from core logic

--- main.go
+++ main.go
@@ -4,242 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
-	"strings"
 
-	"github.com/hamidzr/gmenu/model"
-	"github.com/hamidzr/gmenu/render"
-
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/driver/desktop"
-	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
-	"github.com/sahilm/fuzzy"
+	"github.com/hamidzr/gmenu/core"
 	"github.com/spf13/cobra"
 )
 
-type SearchMethod func([]model.MenuItem, string) []model.MenuItem
-
-func isDirectMatch(item model.MenuItem, keyword string) bool {
-	return strings.Contains(strings.ToLower(item.Title), strings.ToLower(keyword))
-}
-func directSearch(items []model.MenuItem, keyword string) []model.MenuItem {
-	matches := make([]model.MenuItem, 0)
-	for _, item := range items {
-		if isDirectMatch(item, keyword) {
-			matches = append(matches, item)
-		}
-	}
-	return matches
-}
-
-func fuzzySearch(items []model.MenuItem, keyword string) []model.MenuItem {
-	entries := make([]string, len(items))
-	for i, item := range items {
-		entries[i] = item.Title
+func initCLI() *cobra.Command {
+	var RootCmd = &cobra.Command{
+		Use:   "gmenu",
+		Short: "gmenu is a fuzzy menu selector",
+		Run: func(cmd *cobra.Command, args []string) {
+			run()
+		},
 	}
 
-	matches := fuzzy.Find(keyword, entries)
-
-	sort.Slice(matches, func(i, j int) bool {
-		return matches[i].Score > matches[j].Score
-	})
-
-	result := make([]model.MenuItem, 0)
-	for _, match := range matches {
-		result = append(result, items[match.Index])
-	}
-
-	return result
-}
-
-type Menu struct {
-	Items    []model.MenuItem
-	Filtered []model.MenuItem
-	// zero-based index of the selected item in the filtered list
-	Selected int
-	Query    string
-	// ResultText   string
-	// MatchCount is the number of items that matched the search query.
-	MatchCount   int
-	SearchMethod SearchMethod
-	resultLimit  int
-}
-
-func NewMenu(itemTitles []string) Menu {
-	items := make([]model.MenuItem, len(itemTitles))
-	for i, entry := range itemTitles {
-		items[i] = model.MenuItem{Title: entry}
-	}
-	if len(items) == 0 {
-		panic("Menu must have at least one item")
-	}
-
-	m := Menu{Items: items,
-		Selected:     0,
-		SearchMethod: fuzzySearch,
-		resultLimit:  10,
-	}
-	m.Search("")
-	return m
-}
-
-// UpdateItems overrides the menu items.
-func (m *Menu) UpdateItems(itemTitles []string) {
-	items := make([]model.MenuItem, len(itemTitles))
-	for i, entry := range itemTitles {
-		items[i] = model.MenuItem{Title: entry}
-	}
-	m.Items = items
-	m.Search(m.Query)
-}
-
-// AddItems adds the given items to the menu.
-func (m *Menu) AddItems(itemTitles []string) {
-	items := make([]model.MenuItem, len(itemTitles))
-	for i, entry := range itemTitles {
-		items[i] = model.MenuItem{Title: entry}
-	}
-	m.Items = append(m.Items, items...)
-	m.Search(m.Query)
-}
-
-// Filters the menu filtered list to only include items that match the keyword.
-func (m *Menu) Search(keyword string) {
-	m.Query = keyword
-	if keyword == "" {
-		m.Filtered = m.Items
-	} else {
-		m.Filtered = m.SearchMethod(m.Items, keyword)
-	}
-	if len(m.Filtered) > 0 {
-		m.Selected = 0
-	} else {
-		m.Selected = -1
-	}
-	m.MatchCount = len(m.Filtered)
-	if len(m.Filtered) > m.resultLimit {
-		m.Filtered = m.Filtered[:m.resultLimit]
-	}
-}
-
-// SetItems sets the menu items again.
-func (m *Menu) SetItems(itemTitles []string) {
-	items := make([]model.MenuItem, len(itemTitles))
-	for i, entry := range itemTitles {
-		items[i] = model.MenuItem{Title: entry}
-	}
-	m.Items = items
-	// TODO: sync and search
-	m.Search(m.Query)
-}
-
-type GMenu struct {
-	menu *Menu
-	app  fyne.App
-}
-
-// NewGMenu creates a new GMenu instance.
-func NewGMenu(initialItems []string) *GMenu {
-	menu := NewMenu(initialItems)
-	g := &GMenu{}
-	g.menu = &menu
-	g.setupUI()
-	return g
-}
-
-// setupUI creates the UI elements.
-func (g *GMenu) setupUI() {
-	exitCode := 0
-	appTitle := "gmenu"
-	myApp := app.New()
-	g.app = myApp
-	myApp.Settings().SetTheme(render.MainTheme{theme.DefaultTheme()})
-
-	var myWindow fyne.Window
-	if deskDriver, ok := myApp.Driver().(desktop.Driver); ok {
-		myWindow = deskDriver.CreateSplashWindow()
-	} else {
-		myWindow = myApp.NewWindow("")
-	}
-	myWindow.SetTitle(appTitle)
-	pidFile := createPidFile(appTitle)
-
-	searchEntry := &CustomEntry{}
-	searchEntry.ExtendBaseWidget(searchEntry)
-	searchEntry.SetPlaceHolder("Search")
-	mainContainer := container.NewVBox(searchEntry)
-	myWindow.SetContent(mainContainer)
-
-	itemsCanvas := render.NewItemsCanvas()
-	itemsCanvas.Render(g.menu.Filtered, g.menu.Selected)
-	// show match items out of total item count "Matched Items: [10/10]"
-	matchCounterLabel := func() string {
-		return fmt.Sprintf("Matched Items: [%d/%d]", g.menu.MatchCount, len(g.menu.Items))
-	}
-	menuLabel := widget.NewLabel(matchCounterLabel())
-
-	searchEntry.OnChanged = func(text string) {
-		g.menu.Search(text)
-		menuLabel.SetText(matchCounterLabel())
-		itemsCanvas.Render(g.menu.Filtered, g.menu.Selected)
-	}
-	keyHandler := func(key *fyne.KeyEvent) {
-		switch key.Name {
-		case fyne.KeyDown:
-			if g.menu.Selected < len(g.menu.Filtered)-1 {
-				g.menu.Selected++
-			}
-		case fyne.KeyUp:
-			if g.menu.Selected > 0 {
-				g.menu.Selected--
-			}
-		case fyne.KeyReturn:
-			if g.menu.Selected >= 0 && g.menu.Selected < len(g.menu.Filtered)+1 {
-				fmt.Fprintln(os.Stdout, g.menu.Filtered[g.menu.Selected].Title)
-			} else {
-				// TODO: cli option.
-				fmt.Fprintln(os.Stdout, g.menu.Query)
-			}
-			myApp.Quit()
-		case fyne.KeyEscape:
-			exitCode = 1
-			myApp.Quit()
-
-		}
-		itemsCanvas.Render(g.menu.Filtered, g.menu.Selected)
-	}
-	searchEntry.onKeyDown = keyHandler
-	myWindow.Canvas().SetOnTypedKey(keyHandler)
-
-	resultsContainer := container.NewBorder(nil, nil, nil, nil, menuLabel, itemsCanvas.Label)
-	mainContainer.Add(resultsContainer)
-	myWindow.Resize(fyne.NewSize(800, 300))
-	myWindow.SetOnClosed(func() {
-		os.Remove(pidFile)
-		os.Exit(exitCode)
-	}) // Ensure the application exits properly
-
-	// Set focus to the search entry on startup
-	// searchEntry.FocusGained()
-	myWindow.Canvas().Focus(searchEntry)
-	myWindow.Show()
-}
-
-// CustomEntry is a widget.Entry that captures certain key events.
-type CustomEntry struct {
-	widget.Entry
-	onKeyDown func(key *fyne.KeyEvent)
-}
-
-func (e *CustomEntry) TypedKey(key *fyne.KeyEvent) {
-	if e.onKeyDown != nil {
-		e.onKeyDown(key)
-	}
-	// Call the parent method to ensure regular input handling.
-	e.Entry.TypedKey(key)
+	return RootCmd
 }
 
 func readItems() []string {
@@ -266,39 +45,10 @@ func readItems() []string {
 	return items
 }
 
-func createPidFile(name string) string {
-	dir := os.TempDir()
-	pidFile := fmt.Sprintf("%s/%s.pid", dir, name)
-	if _, err := os.Stat(pidFile); err == nil {
-		fmt.Println("Another instance of gmenu is already running. Exiting.")
-		fmt.Println("If this is not the case, please delete the pid file:", pidFile)
-		os.Exit(1)
-	}
-	f, err := os.Create(pidFile)
-	if err != nil {
-		fmt.Println("Failed to create pid file")
-		os.Exit(1)
-	}
-	defer f.Close()
-	return pidFile
-}
-
-func initCLI() *cobra.Command {
-	var RootCmd = &cobra.Command{
-		Use:   "gmenu",
-		Short: "gmenu is a fuzzy menu selector",
-		Run: func(cmd *cobra.Command, args []string) {
-			run()
-		},
-	}
-
-	return RootCmd
-}
-
 func run() {
 	items := readItems()
-	gmenu := NewGMenu(items)
-	gmenu.app.Run()
+	gmenu := core.NewGMenu(items)
+	gmenu.Run()
 }
 
 func main() {
